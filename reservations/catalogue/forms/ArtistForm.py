cb956f0fefc66f816009bcb1f7ce5196171bc7d1
jpmpindu <jpmpindu@gmail.com>
2025-11-02 03:31:16 +0100

    Form update Artist

---
+++ reservations/catalogue/forms/ArtistForm.py
@@ -0,0 +1,12 @@
+from django import forms
+from  catalogue.models import Artist
+
+class ArtistForm(forms.ModelForm):
+
+    class Meta:
+        model = Artist
+
+        fields = [
+            'firstname',
+            'lastname',
+        ]
