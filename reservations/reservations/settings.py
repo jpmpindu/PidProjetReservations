613a62e31dc78b3f577b2f93d526b8910ee620d7
jpmpindu <jpmpindu@gmail.com>
2025-11-18 23:16:18 +0100

    Front authentication - logout

--- reservations/reservations/settings.py
+++ reservations/reservations/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_extensions',
     'catalogue',
 ]
 
@@ -132,3 +133,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/accounts/login'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
