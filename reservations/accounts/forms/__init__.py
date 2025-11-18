50f5b0e8e01e67ce5abcb8327c6607e0db2453f0
jpmpindu <jpmpindu@gmail.com>
2025-11-18 23:56:21 +0100

    Frontend authentication – Advanced signup

---
+++ reservations/accounts/forms/__init__.py
@@ -0,0 +1,1 @@
+from .UserSignupForm import *
--- reservations/accounts/views.py
+++ reservations/accounts/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.shortcuts import redirect
 from django.contrib import messages
+from .forms import UserSignUpForm
 
 
 # Create your views here.
 class UserSignUpView(UserPassesTestMixin, CreateView):
-    form_class = UserCreationForm
+    form_class = UserSignUpForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
