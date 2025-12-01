1fa9547a81b8a6ca8186be3787220008c02c6e01
jpmpindu <jpmpindu@gmail.com>
2025-12-01 23:42:30 +0100

    Données initiales Location avec clés naturelles Locality

--- reservations/catalogue/models/locality.py
+++ reservations/catalogue/models/locality.py
@@ -1,11 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class LocalityManager(models.Manager):
+    def get_by_natural_key(self, postal_code, locality):
+        return self.get(postal_code=postal_code, locality=locality)
+
+
+
 class Locality(models.Model):
     postal_code = models.CharField(max_length=6)
     locality = models.CharField(max_length=60)
+
+    objects = LocalityManager()
+
     def __str__(self):
         return self.type
 
     class Meta:
         db_table = "localities"
+        constraints = [
+            models.UniqueConstraint(
+                fields=["postal_code", "locality"],
+                name="unique_postal_code_locality",
+            ),
+        ]
+    def natural_key(self):
+        return (self.postal_code, self.locality)
